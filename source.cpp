a12d05300a0c0e35241fbd51f2964f833748fc30
夢見りあむ <kengsini250@gmail.com>
2021-06-12 12:45:33 +0800

    Update source.cpp

--- source.cpp
+++ source.cpp
@@ -26,3 +26,44 @@ bool MainWindow::getFiles(const QString &path, QVector<QString>& files)
 
 	return true;
 }
+
+
+//获取图片路径
+void getFiles(string path, vector<string>& files) {
+	//文件句柄	
+	intptr_t hFile = 0;
+	//文件信息	
+	struct _finddata_t fileinfo;
+	string p;
+	if ((hFile = _findfirst(p.assign(path).append("\\*").c_str(), &fileinfo)) != -1)
+	{
+		do
+		{			//如果是目录,迭代之			
+					//如果不是,加入列表			
+			if ((fileinfo.attrib &  _A_SUBDIR))
+			{
+				if (strcmp(fileinfo.name, ".") != 0 && strcmp(fileinfo.name, "..") != 0)
+					getFiles(p.assign(path).append("\\").append(fileinfo.name), files);
+			}
+			else
+			{
+				files.push_back(p.assign(path).append("\\").append(fileinfo.name));
+			}
+		} while (_findnext(hFile, &fileinfo) == 0);
+		_findclose(hFile);
+	}
+}
+
+
+//获取文件名
+string GetImageName(string path)
+{
+	string ImageName;
+	int first = path.find_last_of("\\");
+	int last = path.find_first_of(".");
+	if (first != -1)
+	{
+		ImageName = path.substr(first + 1, last - first - 1);
+	}
+	return ImageName;
+}
